42acfaeec966ecb0a05a82a581c8c2e1b569e450
Ramsi97 <ramadan2jemal@gmail.com>
2025-05-02 07:52:51 +0300

    database connection is worked

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,24 +1,28 @@
 <?php
 
-
-$dbh = "mysql:host=localhost;dbname:contact_form_db";
+$dbh = "mysql:host=localhost;dbname=contact_form_db";
 $dbusername ="root";
 $dbpwd = "";
 
 $username = $_POST["uname"];
 $email = $_POST["email"];
 $text = $_POST["text"];
-echo $username;
-echo $email;
-echo $text;
+
 
 try{
 
     $pdo = new PDO($dbh, $dbusername, $dbpwd);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "INSERT INTO submissions (name, email, message) VALUES(?, ?, ?);";
+    
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$username, $email, $text]);
+
+    $pdo = null;
+    $stmt = null;
+    
 }catch(PDOException $e){
-
     echo $e->getMessage();
-
 }
 
